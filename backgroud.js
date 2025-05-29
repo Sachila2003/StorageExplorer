2918abe80a2d0292bac2794c60baeb5091764d0e
Sachila2003 <sachilageeth1@gmail.com>
2025-05-29 23:26:11 +0530

    1st comiit

---
+++ backgroud.js
@@ -0,0 +1,16 @@
+chrome.runtime.onInstalled.addListener((details) => {
+    console.log("Web Storage Manager extension has been installed or updated.");
+
+    if (details.reason === 'install') {
+        console.log('This is a first install of Web Storage Manager!');
+    }else if(details.reason === 'update'){
+        console.log('Web Storage Manager extension has been updated to version:', chrome.runtime.getManifest().version);
+    }
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>{
+    console.log("Background script received a message: ", message);
+    console.log("From sender: ", sender);
+})
+
+console.log("Tab Stash Background Service Worker started or awakened.");
